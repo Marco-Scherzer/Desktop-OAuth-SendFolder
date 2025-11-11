fbce4618915e18292177cc4fc6fd94cd7fdcf78a
Marco Scherzer <m.scherzer@hotmail.com>
2025-11-12 00:42:01 +0100

    preAlpha 0.1 - between state

---
+++ src/main/java/com/marcoscherzer/oauthfilesendfolderforgooglemail/MConsentOutgoingMailWatcher.java
@@ -0,0 +1,175 @@
+package com.marcoscherzer.oauthfilesendfolderforgooglemail;
+
+import com.marcoscherzer.msimplegooglemailer.MOutgoingMail;
+import com.marcoscherzer.msimplegooglemailer.MSimpleGoogleMailer;
+
+import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.util.concurrent.*;
+
+/**
+ * @author Marco Scherzer, Copyright Marco Scherzer, All rights reserved
+ */
+public class MConsentOutgoingMailWatcher extends MFolderWatcher{
+
+    private final String fromAddress;
+    private final String toAddress;
+    private final String clientAndPathUUID;
+    private final boolean askConsent;
+    private final Path notSentFolder;
+    private final MSimpleGoogleMailer mailer;
+    private final Path sentFolder;
+    private volatile boolean userConsentActive = false;
+    private final ScheduledExecutorService consentTimer = Executors.newSingleThreadScheduledExecutor();
+    private ScheduledFuture<?> pendingSessionEnd = null;
+    private final List<MOutgoingMail> toSendMails = Collections.synchronizedList(new ArrayList<>());
+    private final List<MOutgoingMail> sentMails = Collections.synchronizedList(new ArrayList<>());
+    private final long sessionIdleMillis = 15000;
+    private final Object consentLock = new Object();
+
+    /**
+     * @author Marco Scherzer, Copyright Marco Scherzer, All rights reserved
+     */
+    public MConsentOutgoingMailWatcher(Path outFolder, Path sentFolder, Path notSentFolder, MSimpleGoogleMailer mailer,
+                                       String fromAddress,
+                                       String toAddress,
+                                       String clientAndPathUUID,
+                                       boolean askConsent) {
+        super(outFolder);
+        this.sentFolder = sentFolder;
+        this.fromAddress = fromAddress;
+        this.toAddress = toAddress;
+        this.clientAndPathUUID = clientAndPathUUID;
+        this.askConsent = askConsent;
+        this.notSentFolder = notSentFolder;
+        this.mailer = mailer;
+    }
+
+    /**
+     * @author Marco Scherzer, Copyright Marco Scherzer, All rights reserved
+     */
+    public void onFileChangedAndUnlocked(Path file) {
+        if (!Files.exists(file)) {
+            System.out.println("User deleted file. Nothing to send.");
+            return;
+        }
+
+        String sendDateTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        MOutgoingMail mail = new MOutgoingMail(fromAddress, toAddress, clientAndPathUUID + ", sendTime " + sendDateTime)
+                .appendMessageText(clientAndPathUUID + "\\" + file.getFileName())
+                .addAttachment(file.toString());
+
+        synchronized (consentLock) {
+            if (!askConsent || userConsentActive) {
+                toSendMails.add(mail);
+                scheduleSessionEnd();
+                return;
+            }
+
+            if (showSendAlert()) {
+                userConsentActive = true;
+                toSendMails.add(mail);
+                scheduleSessionEnd();
+            } else {
+                try {
+                    Path targetFile = notSentFolder.resolve(file.getFileName());
+                    Files.move(file, targetFile, StandardCopyOption.REPLACE_EXISTING);
+                    System.out.println("User denied sending mail. File moved to NotSent: " + targetFile);
+                } catch (Exception moveExc) {
+                    System.err.println("Error while moving to NotSent: " + moveExc.getMessage());
+                }
+            }
+        }
+    }
+
+    /**
+     * @author Marco Scherzer, Copyright Marco Scherzer, All rights reserved
+     */
+    private void scheduleSessionEnd() {
+        if (pendingSessionEnd != null && !pendingSessionEnd.isDone()) {
+            pendingSessionEnd.cancel(false);
+        }
+
+        pendingSessionEnd = consentTimer.schedule(() -> {
+            synchronized (consentLock) {
+                List<MOutgoingMail> successfullySent = new ArrayList<>();
+
+                try {
+
+                    SimpleDateFormat timestampFormat = new SimpleDateFormat("yyyyMMdd_HHmmss_SSS");
+
+                    for (MOutgoingMail ml : toSendMails) {
+                        File sourceFile = new File(ml.getAttachment(0));
+                        String originalName = sourceFile.getName();
+                        String extension = "";
+                        int dotIndex = originalName.lastIndexOf('.');
+                        if (dotIndex != -1) {
+                            extension = originalName.substring(dotIndex);
+                            originalName = originalName.substring(0, dotIndex);
+                        }
+                        String timestamp = timestampFormat.format(new Date());
+                        String newName = originalName + "_" + timestamp + extension;
+
+                        try {
+                            mailer.send(ml);
+
+                            File targetFile = new File(sentFolder.toFile(), newName);
+                            Files.move(sourceFile.toPath(), targetFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+                            System.out.println("Sent and moved: " + newName);
+
+                            successfullySent.add(ml);
+                            sentMails.add(ml);
+                        } catch (Exception e) {
+                            try {
+                                File fallbackFile = new File(notSentFolder.toFile(), newName);
+                                Files.move(sourceFile.toPath(), fallbackFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+                                System.err.println("Send failed, moved to NOT SENT: " + newName);
+                            } catch (IOException moveFail) {
+                                System.err.println("Failed to move unsent file: " + sourceFile.getName() + " -> " + moveFail.getMessage());
+                            }
+                            System.err.println("Error sending: " + sourceFile.getName() + " -> " + e.getMessage());
+                        }
+                    }
+
+                    toSendMails.removeAll(successfullySent);
+                    System.out.println("Mails sent: " + successfullySent.size());
+                } catch (Exception sendExc) {
+                    System.err.println("Error during session end: " + sendExc.getMessage());
+                }
+
+                userConsentActive = false;
+            }
+        }, sessionIdleMillis, TimeUnit.MILLISECONDS);
+    }
+
+    /**
+     * @author Marco Scherzer, Copyright Marco Scherzer, All rights reserved
+     */
+    private boolean showSendAlert() {
+        JFrame frame = new JFrame();
+        frame.setAlwaysOnTop(true);
+        frame.setUndecorated(true);
+        frame.setLocationRelativeTo(null);
+
+        int result = JOptionPane.showConfirmDialog(
+                frame,
+                "Do you want to send the mail?",
+                "Send Mail",
+                JOptionPane.YES_NO_OPTION,
+                JOptionPane.QUESTION_MESSAGE
+        );
+
+        frame.dispose();
+        return result == JOptionPane.YES_OPTION;
+    }
+
+
+}
--- src/main/java/com/marcoscherzer/oauthfilesendfolderforgooglemail/MSimpleGoogleMailerService.java
+++ src/main/java/com/marcoscherzer/oauthfilesendfolderforgooglemail/MSimpleGoogleMailerService.java
@@ -1,26 +1,15 @@
 package com.marcoscherzer.oauthfilesendfolderforgooglemail;
 
-import com.marcoscherzer.msimplegooglemailer.MOutgoingMail;
 import com.marcoscherzer.msimplegooglemailer.MSimpleGoogleMailer;
 import com.marcoscherzer.msimplegooglemailer.MSimpleKeystore;
 
-import javax.swing.*;
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.*;
-import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.*;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.TimeUnit;
 
 import static com.marcoscherzer.oauthfilesendfolderforgooglemail.MUtil.createFolderLink;
 import static com.marcoscherzer.oauthfilesendfolderforgooglemail.MUtil.createPathIfNotExists;
 import static com.marcoscherzer.msimplegooglemailer.MSimpleGoogleMailerUtil.checkMailAddress;
-
 
 /**
  * @author Marco Scherzer, Copyright Marco Scherzer, All rights reserved
@@ -29,17 +18,16 @@ public final class MSimpleGoogleMailerService {
 
     private static final String userDir = System.getProperty("user.dir");
     private static final String basePath = userDir+"\\mail";
-    private static String clientAndPathUUID;
-    private static MFolderWatcher watcher;
-    private static Path sentFolder;
-    private static Path outFolder;
-    private static String fromAdress;
-    private static String toAdress;
     private static MFileNameWatcher outgoingDesktopLinkWatcher;
     private static MFileNameWatcher sentDesktopLinkWatcher;
     private static boolean askConsent = true;
-
-
+    private static Path notSentFolder;
+    private static Path sentFolder;
+    private static Path outFolder;
+    private static String clientAndPathUUID;
+    private static MFolderWatcher watcher;
+    private static String fromAddress;
+    private static String toAddress;
 
     /**
      * @author Marco Scherzer, Copyright Marco Scherzer, All rights reserved
@@ -70,130 +58,28 @@ public final class MSimpleGoogleMailerService {
             Path outPath = Paths.get(basePath, clientAndPathUUID);
             Path sentPath = Paths.get(basePath, clientAndPathUUID + "-sent");
             Path notSentPath = Paths.get(basePath, clientAndPathUUID + "-notSent");
+/*
 
+                    sentFolder.mkdirs();
+                    notSentFolder.toFile().mkdirs();
+ */
             outFolder = createPathIfNotExists(outPath, "Out folder");
             sentFolder = createPathIfNotExists(sentPath, "Sent folder");
-            Path notSentFolder = createPathIfNotExists(notSentPath, "NotSent folder");
+            notSentFolder = createPathIfNotExists(notSentPath, "NotSent folder");
 
-            fromAdress = store.get("fromAddress");
-            toAdress = store.get("toAddress");
+            fromAddress = store.get("fromAddress");
+            toAddress = store.get("toAddress");
 
-
-            /**
-             * @author Marco Scherzer, Copyright Marco Scherzer, All rights reserved
-             */
-            watcher = new MFolderWatcher(outFolder) {
-                private volatile boolean userConsentActive = false;
-                private final ScheduledExecutorService consentTimer = Executors.newSingleThreadScheduledExecutor();
-                private ScheduledFuture<?> pendingSessionEnd = null;
-                private final List<MOutgoingMail> toSendMails = Collections.synchronizedList(new ArrayList<>());
-                private final List<MOutgoingMail> sentMails = Collections.synchronizedList(new ArrayList<>());
-                private final long sessionIdleMillis = 15000; // 15 Sekunden Ruhezeit
-                private final Object consentLock = new Object(); // Synchronisierung
-
-                /**
-                 * @author Marco Scherzer, Copyright Marco Scherzer, All rights reserved
-                 */
-                @Override
-                protected final void onFileChangedAndUnlocked(Path file) {
-                    if (!Files.exists(file)) {
-                        System.out.println("User deleted file. Nothing to send.");
-                        return;
-                    }
-
-                    String sendDateTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-                    MOutgoingMail mail = new MOutgoingMail(fromAdress, toAdress, clientAndPathUUID + ", sendTime " + sendDateTime)
-                            .appendMessageText(clientAndPathUUID + "\\" + file.getFileName())
-                            .addAttachment(file.toString());
-
-                    synchronized (consentLock) {
-                        if (!askConsent || userConsentActive) {
-                            toSendMails.add(mail);
-                            scheduleSessionEnd();
-                            return;
-                        }
-
-                        if (showSendAlert()) {
-                            userConsentActive = true;
-                            toSendMails.add(mail);
-                            scheduleSessionEnd();
-                        } else {
-                            try {
-                                Path targetFile = notSentFolder.resolve(file.getFileName());
-                                Files.move(file, targetFile, StandardCopyOption.REPLACE_EXISTING);
-                                System.out.println("User denied sending mail. File moved to NotSent: " + targetFile);
-                            } catch (Exception moveExc) {
-                                System.err.println("Error while moving to NotSent: " + moveExc.getMessage());
-                            }
-                        }
-                    }
-                }
-
-                /**
-                 * @author Marco Scherzer, Copyright Marco Scherzer, All rights reserved
-                 */
-                private void scheduleSessionEnd() {
-                    if (pendingSessionEnd != null && !pendingSessionEnd.isDone()) {
-                        pendingSessionEnd.cancel(false);
-                    }
-
-                    pendingSessionEnd = consentTimer.schedule(() -> {
-                        synchronized (consentLock) {
-                            List<MOutgoingMail> successfullySent = new ArrayList<>();
-
-                            try {
-                                File sentFolder = new File("D:\\User\\Marco Scherzer\\Desktop\\Sent Mails");
-                                File notSentFolder = new File("D:\\User\\Marco Scherzer\\Desktop\\Not Sent Mails");
-                                sentFolder.mkdirs();
-                                notSentFolder.mkdirs();
-
-                                SimpleDateFormat timestampFormat = new SimpleDateFormat("yyyyMMdd_HHmmss_SSS");
-
-                                for (MOutgoingMail ml : toSendMails) {
-                                    File sourceFile = new File(ml.getAttachment(0)) ;
-                                    String originalName = sourceFile.getName();
-                                    String extension = "";
-                                    int dotIndex = originalName.lastIndexOf('.');
-                                    if (dotIndex != -1) {
-                                        extension = originalName.substring(dotIndex);
-                                        originalName = originalName.substring(0, dotIndex);
-                                    }
-                                    String timestamp = timestampFormat.format(new Date());
-                                    String newName = originalName + "_" + timestamp + extension;
-
-                                    try {
-                                        mailer.send(ml); // dein echter Versand
-
-                                        File targetFile = new File(sentFolder, newName);
-                                        Files.move(sourceFile.toPath(), targetFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
-                                        System.out.println("Sent and moved: " + newName);
-
-                                        successfullySent.add(ml);
-                                        sentMails.add(ml);
-                                    } catch (Exception e) {
-                                        try {
-                                            File fallbackFile = new File(notSentFolder, newName);
-                                            Files.move(sourceFile.toPath(), fallbackFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
-                                            System.err.println("Send failed, moved to NOT SENT: " + newName);
-                                        } catch (IOException moveFail) {
-                                            System.err.println("Failed to move unsent file: " + sourceFile.getName() + " -> " + moveFail.getMessage());
-                                        }
-                                        System.err.println("Error sending: " + sourceFile.getName() + " -> " + e.getMessage());
-                                    }
-                                }
-
-                                toSendMails.removeAll(successfullySent);
-                                System.out.println("Mails sent: " + successfullySent.size());
-                            } catch (Exception sendExc) {
-                                System.err.println("Error during session end: " + sendExc.getMessage());
-                            }
-
-                            userConsentActive = false;
-                        }
-                    }, sessionIdleMillis, TimeUnit.MILLISECONDS);
-                }
-            };
-
+            MConsentOutgoingMailWatcher watcher = new MConsentOutgoingMailWatcher(
+                    outFolder,
+                    sentFolder,
+                    notSentFolder,
+                    mailer,
+                    fromAddress,
+                    toAddress,
+                    clientAndPathUUID,
+                    askConsent
+            );
 
             watcher.startWatching();
 
@@ -207,47 +93,9 @@ public final class MSimpleGoogleMailerService {
                 }
             }
 
-            Path absoluteOutgoingLinkPath = createFolderLink(outFolder.toString(), "Outgoing Things");
-            Path absoluteSentLinkPath = createFolderLink(sentFolder.toString(), "Sent Things");
-            Path absoluteNotSentLinkPath = createFolderLink(notSentFolder.toString(), "NotSent Things");
+            createDesktopLinksAndStartWatchingNames();
 
-            if (absoluteOutgoingLinkPath != null || absoluteSentLinkPath != null || absoluteNotSentLinkPath != null) {
-
-                outgoingDesktopLinkWatcher = new MFileNameWatcher(absoluteOutgoingLinkPath) {
-                    @Override
-                    protected void onFileNameChanged(WatchEvent.Kind<?> kind, Path fileName) {
-                        System.out.println("Security integrity violated. Desktop Folder Link \"" + absoluteOutgoingLinkPath + "\" changed. Shutting down.");
-                        exit(0);
-                    }
-                };
-
-                sentDesktopLinkWatcher = new MFileNameWatcher(absoluteSentLinkPath) {
-                    @Override
-                    protected void onFileNameChanged(WatchEvent.Kind<?> kind, Path fileName) {
-                        System.out.println("Security integrity violated. Desktop Folder Link \"" + absoluteSentLinkPath + "\" changed. Shutting down.");
-                        exit(0);
-                    }
-                };
-
-                MFileNameWatcher notSentDesktopLinkWatcher = new MFileNameWatcher(absoluteNotSentLinkPath) {
-                    @Override
-                    protected void onFileNameChanged(WatchEvent.Kind<?> kind, Path fileName) {
-                        System.out.println("Security integrity violated. Desktop Folder Link \"" + absoluteNotSentLinkPath + "\" changed. Shutting down.");
-                        exit(0);
-                    }
-                };
-
-                outgoingDesktopLinkWatcher.startWatching();
-                System.out.println("Monitoring \"" + absoluteOutgoingLinkPath + "\" for name integrity violations...");
-                sentDesktopLinkWatcher.startWatching();
-                System.out.println("Monitoring \"" + absoluteSentLinkPath + "\" for name integrity violations...");
-                notSentDesktopLinkWatcher.startWatching();
-                System.out.println("Monitoring \"" + absoluteNotSentLinkPath + "\" for name integrity violations...");
-            } else {
-                System.out.println("No Desktop links could be created. Please create links manually");
-            }
-
-            printConfiguration(fromAdress, toAdress, basePath, clientAndPathUUID, clientAndPathUUID + "-sent");
+            printConfiguration(fromAddress, toAddress, basePath, clientAndPathUUID, clientAndPathUUID + "-sent");
 
             System.out.println("To end the Program please press a key");
             new Scanner(System.in).nextLine();
@@ -256,28 +104,6 @@ public final class MSimpleGoogleMailerService {
             exc.printStackTrace();
             exit(1);
         }
-    }
-
-
-    /**
-     * @author Marco Scherzer, Copyright Marco Scherzer, All rights reserved
-     */
-    private static boolean showSendAlert() {
-        JFrame frame = new JFrame();
-        frame.setAlwaysOnTop(true);
-        frame.setUndecorated(true);
-        frame.setLocationRelativeTo(null);
-
-        int result = JOptionPane.showConfirmDialog(
-                frame,
-                "Do you want to send the mail ?",
-                "Send Mail",
-                JOptionPane.YES_NO_OPTION,
-                JOptionPane.QUESTION_MESSAGE
-        );
-
-        frame.dispose(); // sauber schließen
-        return result == JOptionPane.YES_OPTION;
     }
 
 
@@ -319,6 +145,51 @@ public final class MSimpleGoogleMailerService {
     }
 
     /**
+     * @author Marco Scherzer, Copyright Marco Scherzer, All rights reserved
+     */
+    private static void  createDesktopLinksAndStartWatchingNames() throws Exception {
+        Path absoluteOutgoingLinkPath = createFolderLink(outFolder.toString(), "Outgoing Things");
+        Path absoluteSentLinkPath = createFolderLink(sentFolder.toString(), "Sent Things");
+        Path absoluteNotSentLinkPath = createFolderLink(notSentFolder.toString(), "NotSent Things");
+
+        if (absoluteOutgoingLinkPath != null || absoluteSentLinkPath != null || absoluteNotSentLinkPath != null) {
+
+            outgoingDesktopLinkWatcher = new MFileNameWatcher(absoluteOutgoingLinkPath) {
+                @Override
+                protected void onFileNameChanged(WatchEvent.Kind<?> kind, Path fileName) {
+                    System.out.println("Security integrity violated. Desktop Folder Link \"" + absoluteOutgoingLinkPath + "\" changed. Shutting down.");
+                    exit(0);
+                }
+            };
+
+            sentDesktopLinkWatcher = new MFileNameWatcher(absoluteSentLinkPath) {
+                @Override
+                protected void onFileNameChanged(WatchEvent.Kind<?> kind, Path fileName) {
+                    System.out.println("Security integrity violated. Desktop Folder Link \"" + absoluteSentLinkPath + "\" changed. Shutting down.");
+                    exit(0);
+                }
+            };
+
+            MFileNameWatcher notSentDesktopLinkWatcher = new MFileNameWatcher(absoluteNotSentLinkPath) {
+                @Override
+                protected void onFileNameChanged(WatchEvent.Kind<?> kind, Path fileName) {
+                    System.out.println("Security integrity violated. Desktop Folder Link \"" + absoluteNotSentLinkPath + "\" changed. Shutting down.");
+                    exit(0);
+                }
+            };
+
+            outgoingDesktopLinkWatcher.startWatching();
+            System.out.println("Monitoring \"" + absoluteOutgoingLinkPath + "\" for name integrity violations...");
+            sentDesktopLinkWatcher.startWatching();
+            System.out.println("Monitoring \"" + absoluteSentLinkPath + "\" for name integrity violations...");
+            notSentDesktopLinkWatcher.startWatching();
+            System.out.println("Monitoring \"" + absoluteNotSentLinkPath + "\" for name integrity violations...");
+        } else {
+            System.out.println("No Desktop links could be created. Please create links manually");
+        }
+    }
+
+    /**
      * @author Marco Scherzer
      * Copyright © Marco Scherzer. All rights reserved.
      */
@@ -349,11 +220,6 @@ public final class MSimpleGoogleMailerService {
         );
 
     }
-
-
-
-
-
 
 }
 
