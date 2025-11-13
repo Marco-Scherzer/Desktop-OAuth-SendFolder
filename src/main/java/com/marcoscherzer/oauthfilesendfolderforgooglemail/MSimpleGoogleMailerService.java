24155d67b4a59add1de07e34432a9961b4470522
Marco Scherzer <m.scherzer@hotmail.com>
2025-11-13 15:37:06 +0100

    preAlpha 0.1 - between state

--- src/main/java/com/marcoscherzer/oauthfilesendfolderforgooglemail/MSimpleGoogleMailerService.java
+++ src/main/java/com/marcoscherzer/oauthfilesendfolderforgooglemail/MSimpleGoogleMailerService.java
@@ -50,7 +50,7 @@ public final class MSimpleGoogleMailerService {
             System.out.println();
             MSimpleGoogleMailer.setClientKeystoreDir(userDir);
             //MSimpleGoogleMailer mailer = new MSimpleGoogleMailer("BackupMailer", pw, false); //dbg
-            mailer = new MSimpleGoogleMailer("BackupMailer", pw, false);//false because of dbg
+            mailer = new MSimpleGoogleMailer("BackupMailer", pw, true);//false because of dbg
             MSimpleKeystore store = mailer.getKeystore();
 
             if (!store.containsAllNonNullKeys("fromAddress", "toAddress")) {
